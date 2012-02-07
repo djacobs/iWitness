270ef6e34975b97c1eb62215380e7c859d823e1f
John Andrews + Adam McCrea <pairing+ja+am@edgecase.com>
2012-02-07 16:15:24 -0500

    use ENV api key on heroku

--- config/initializers/gmaps_config.rb
+++ config/initializers/gmaps_config.rb
@@ -1,1 +1,5 @@
-GMAPS = YAML.load_file(Rails.root.join("config", "gmaps.yml"))
+GMAPS = if ENV['GMAPS_API']
+          { 'api_key' => ENV['GMAPS_API'] }
+        else
+          YAML.load_file(Rails.root.join("config", "gmaps.yml"))
+        end
