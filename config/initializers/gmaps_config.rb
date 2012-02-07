bf5747ea998356ed982afdddcb0a3f1cc8b1edf5
Adam McCrea <adam@adamlogic.com>
2012-02-07 16:52:38 -0500

    handle non-existence of ENV var and config file

--- config/initializers/gmaps_config.rb
+++ config/initializers/gmaps_config.rb
@@ -1,5 +1,5 @@
 GMAPS = if ENV['GMAPS_API']
           { 'api_key' => ENV['GMAPS_API'] }
-        else
+        elsif File.exists?(Rails.root.join("config", "gmaps.yml"))
           YAML.load_file(Rails.root.join("config", "gmaps.yml"))
         end
