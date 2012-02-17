2ee9b1b429942330653e8f0808493d144e9c6ae3
John Andrews + Adam McCrea <pairing+ja+am@edgecase.com>
2012-02-17 13:56:53 -0500

    use grammys as defaults

--- app/assets/javascripts/base.js
+++ app/assets/javascripts/base.js
@@ -5,9 +5,9 @@ IWitness.log = _.bind(Ember.Logger.log, Ember.Logger);
 // fill in test data
 $(function(){
   _.delay(function(){
-    $("#start_date").val("2/12/2012").trigger('change');
-    $("#start_time").val("10:00 PM").trigger('change');
-    $("#end_date").val("2/12/2012").trigger('change');
-    $("#end_time").val("11:00 PM").trigger('change');
+    $("#start_date").val("2/13/2012").trigger('change');
+    $("#start_time").val("1:00 AM").trigger('change');
+    $("#end_date").val("2/13/2012").trigger('change');
+    $("#end_time").val("2:00 PM").trigger('change');
   }, 500);
 });
