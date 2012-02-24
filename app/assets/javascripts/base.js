c4f1f032ba52324f6daa4763d765a33ded1e4954
John Andrews + Mike Enriquez <pairing+ja+me@edgecase.com>
2012-02-24 11:54:02 -0500

    remove default times

--- app/assets/javascripts/base.js
+++ app/assets/javascripts/base.js
@@ -1,15 +1,5 @@
 window.IWitness = Ember.Application.create({
   ready: function() {
-    _.delay(function(){
-      if ($('#start_date').val() == '') {
-        $("#keyword").val("glass").trigger('change');
-        $("#start_date").val("2/14/2012").trigger('change');
-        $("#start_time").val("6:00 PM").trigger('change');
-        $("#end_date").val("2/14/2012").trigger('change');
-        $("#end_time").val("7:00 PM").trigger('change');
-      }
-    }, 500);
-
     IWitness.routes.draw();
   }
 });
