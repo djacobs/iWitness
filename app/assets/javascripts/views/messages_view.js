52b26480f2812f0f91aa0d0e7c4cd9eb9fc925c1
John Andrews + Alex Burkhart <pairing+ja+ab@edgecase.com>
2012-02-29 13:57:02 -0500

    Translate service pending status into 'no results' for display

--- app/assets/javascripts/views/messages_view.js
+++ app/assets/javascripts/views/messages_view.js
@@ -1,6 +1,18 @@
 IWitness.MessagesView = Ember.View.extend({
   templateName:         'messages_template',
   modelBinding:         'IWitness.criteriaController.content',
-  flickrStatusBinding:  'IWitness.searchController.monitors.flickr.status',
-  twitterStatusBinding: 'IWitness.searchController.monitors.twitter.status'
+
+  translateStatus: function(service){
+    var status = IWitness.searchController.getPath('monitors.'+service+'.status')
+    if (status == 'pending') return 'searching';
+    else return status;
+  },
+
+  flickrStatus: function(){
+    return this.translateStatus('flickr');
+  }.property('IWitness.searchController.monitors.flickr.status'),
+
+  twitterStatus: function(){
+    return this.translateStatus('twitter');
+  }.property('IWitness.searchController.monitors.twitter.status')
 });
