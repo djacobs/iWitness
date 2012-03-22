90a3d7113c54742489d10eab46b126acd97bc41f
Adam McCrea <adam@edgecase.com>
2012-03-22 11:59:25 -0400

    Show status in results status bar

--- app/views/messages_view.js
+++ app/views/messages_view.js
@@ -1,23 +1,5 @@
 IWitness.MessagesView = Ember.View.extend({
   templateName:         'messages_template',
-  modelBinding:         'IWitness.criteriaController.content',
-
-  translateStatus: function(service){
-    var status = IWitness.searchController.getPath('monitors.'+service+'.status')
-    if (status == 'pending'){
-      return this.getPath('model.isValid') ? 'searching' : 'no results';
-    } else {
-      return status;
-    }
-  },
-
-  flickrStatus: function(){
-    return this.translateStatus('flickr');
-  }.property('IWitness.searchController.monitors.flickr.status', 'model.isValid'),
-
-  twitterStatus: function(){
-    return this.translateStatus('twitter');
-  }.property('IWitness.searchController.monitors.twitter.status', 'model.isValid'),
 
   clearCurated: function(){
     IWitness.curatedSetController.clear();
--- app/views/results_status_view.js
+++ app/views/results_status_view.js
@@ -1,3 +1,34 @@
 IWitness.ResultsStatusView = Ember.View.extend({
-  templateName: 'results_status_template'
+  templateName: 'results_status_template',
+  criteriaBinding:         'IWitness.criteriaController.content',
+
+  status: function() {
+    if (this.getPath('criteria.isValid')) {
+      if (['isFlickrSearching', 'isTwitterSearching'].every(this.getPath, this)) {
+        return 'Scanning';
+      } else {
+        return 'Finished';
+      }
+    } else {
+      return "Something's Wrong";
+    }
+  }.property('isFlickrSearching', 'isTwitterSearching', 'criteria.isValid'),
+
+  statusClass: function() {
+    var statusClass = this.get('status').replace(' ','_').replace("'",'').toLowerCase();
+    return 'status ' + statusClass;
+  }.property('status'),
+
+  isFlickrSearching: function(){
+    return this._isSearching('flickr');
+  }.property('IWitness.searchController.monitors.flickr.status', 'criteria.isValid'),
+
+  isTwitterSearching: function(){
+    return this._isSearching('twitter');
+  }.property('IWitness.searchController.monitors.twitter.status', 'criteria.isValid'),
+
+  _isSearching: function(service){
+    var status = IWitness.searchController.getPath('monitors.'+service+'.status')
+    return status == 'pending';
+  }
 });
