0d50b7742a08bdc965bacca8176878ec6efb5d96
John Andrews + Mike Enriquez <pairing+ja+me@edgecase.com>
2012-03-26 14:51:50 -0400

    switch between views

--- app/views/starred_header_view.js
+++ app/views/starred_header_view.js
@@ -1,3 +1,6 @@
 IWitness.StarredHeaderView = Ember.View.extend({
-  templateName: 'starred_header_template'
+  templateName: 'starred_header_template',
+  switchToResults: function(){
+    IWitness.currentViewController.set('currentView', 'search_results');
+  }
 });
--- app/views/starred_map_view.js
+++ app/views/starred_map_view.js
@@ -1,11 +1,15 @@
 IWitness.StarredMapView = Ember.View.extend({
   templateName: 'starred_map_template',
+  currentViewBinding: 'IWitness.currentViewController',
   // selectedResultBinding: 'IWitness.starredSetController.selectedResult',
 
   didInsertElement: function() {
-    var self = this;
-    window.MAP = this.map = new Map(document.getElementById("starred-map"), 40.735955030904755, -73.99026397144165); // OWS Union Sq
-  },
+    if (this.getPath('currentView.showingStarredResults') && !this.map) {
+      _.defer(function(){
+        this.map = new Map(document.getElementById("starred-map"), 40.735955030904755, -73.99026397144165); // OWS Union Sq
+      });
+    }
+  }.observes('currentView.showingStarredResults'),
 
   numberOfItems: function(){
     var num = this.getPath('IWitness.starredSetController.length');
