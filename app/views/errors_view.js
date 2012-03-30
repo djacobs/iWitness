dd7debbcf7551e1eb9b71ec94e4759e9088d6a8d
John Andrews + Mike Enriquez <pairing+ja+me@edgecase.com>
2012-03-30 10:12:14 -0400

    remove console log

--- app/views/errors_view.js
+++ app/views/errors_view.js
@@ -3,7 +3,6 @@ IWitness.ErrorsView = Ember.View.extend({
   classNameBindings: ["isError"],
 
   isError: function() {
-    console.log("isError: ", !!this.get("error"));
     return !!this.get("error");
   }.property("error")
 });
