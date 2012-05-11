9ec2aee068276874bcf028e1c12974994f62b83d
John Andrews + Alex Burkhart <pairing+ja+ab@edgecase.com>
2012-05-11 15:12:58 -0400

    wire up filters and back to top

--- app/views/search_view.js
+++ app/views/search_view.js
@@ -1,4 +1,33 @@
 IWitness.SearchView = Ember.View.extend({
   templateName: 'search_template',
-  isVisibleBinding: 'IWitness.currentViewController.showingSearchResults'
+  isVisibleBinding: 'IWitness.currentViewController.showingSearchResults',
+
+  didInsertElement: function() {
+    var button = this.$("#back-to-top-button");
+    button.hide();
+    $(window).on("scroll.backToTop", this._backToTopButton(button));
+  },
+
+  showFilters: function() {
+    this.$("#filter-popover").show();
+  },
+
+  hideFilters: function() {
+    this.$("#filter-popover").hide();
+  },
+
+  backToTop: function() {
+    $(document).scrollTop(0);
+  },
+
+  _backToTopButton: function(button) {
+    return _.debounce(function(e) {
+      if ($(window).scrollTop() > 100) {
+        button.show();
+      } else {
+        button.hide();
+      }
+    }, 100);
+  }
+
 });
---
+++ app/views/service_switch_view.js
@@ -0,0 +1,20 @@
+Ember.ENV.RAISE_ON_DEPRECATION = true;
+IWitness.ServiceSwitchView = Ember.View.extend({
+  templateName: "service_switch_template",
+  classNames: ["row"],
+
+  type: function() {
+    var type = this.getPath("model.type");
+    return type.toUpperCase().charAt(0) + type.substring(1);
+  }.property("model.type"),
+
+  status: function() {
+    if (this.getPath("model.active"))
+      return "service-switch on";
+    return "service-switch off";
+  }.property("model.active"),
+
+  toggle: function() {
+    this.get("model").toggleProperty("active");
+  }
+});
