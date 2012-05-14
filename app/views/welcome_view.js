66db0dab5768ee9e5b78892387448bd613b878ec
John Andrews <john@edgecase.com>
2012-05-14 15:04:35 -0400

    show welcome screen on first load

--- app/views/welcome_view.js
+++ app/views/welcome_view.js
@@ -1,6 +1,13 @@
 IWitness.WelcomeView = Ember.View.extend({
   templateName: "welcome_template",
 
+  didInsertElement: function() {
+    if (!localStorage["didSeeWelcomePage"]) {
+      localStorage["didSeeWelcomePage"] = true;
+      IWitness.currentViewController.set("showWelcome", true);
+    }
+  },
+
   isVisible: function() {
     return IWitness.currentViewController.get("showingWelcomeOverlay");
   }.property("IWitness.currentViewController.showingWelcomeOverlay"),
