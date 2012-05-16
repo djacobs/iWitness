df68b46027764fc9edfb0554b56a01f7e2dcea2b
John Andrews + Adam McCrea <pairing+ja+am@edgecase.com>
2012-05-16 15:25:27 -0400

    hide filter popover when clicked outside

--- app/views/search_view.js
+++ app/views/search_view.js
@@ -3,9 +3,18 @@ IWitness.SearchView = Ember.View.extend({
   isVisibleBinding: 'IWitness.currentViewController.showingSearchResults',
 
   didInsertElement: function() {
+    var self = this;
     var button = this.$("#back-to-top-button");
     button.hide();
     $(window).on("scroll.backToTop", this._backToTopButton(button));
+
+    $(document).click(function(e) {
+      var clicked = $(e.target);
+      if (!clicked.closest('#show-filters-button').length &&
+          !clicked.closest('#filter-popover .content').length) {
+        self.hideFilters();
+      }
+    });
   },
 
   showFilters: function() {
